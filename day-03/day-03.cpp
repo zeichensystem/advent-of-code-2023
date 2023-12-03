257edd8c5a0a824d27ee696aca2ced551c158071
jun <83899451+zeichensystem@users.noreply.github.com>
2023-12-03 13:06:41 +0100

    Improve Day 03
    - Use std::ssize instead of casting to int where necessary
    - Use std::isdigit instead of my own implementation
    - Fix lambda: capture lines as a reference to avoid expensive copying (I thought it was not necessary because lines is already a const ref...)
    - Fix bugs in part 1 and part 2 which didn't trigger on my (and many other) inputs (my solutions were correct by sheer "luck") (cf. Notes)

--- day-03/day-03.cpp
+++ day-03/day-03.cpp
@@ -12,18 +12,19 @@
         - Nice C++20 feature: default struct comparison operators
         - Part 2: - Just one index (either begin_idx or end_idx) would be sufficient for NumInfo
                   - Could use an unordered_set instead of a vector for nums (in get_gear_ratio), but n <= 8 so it doesn't really matter
+                  - Fix lambda: capture lines as reference to avoid expensive copying (I thought it was not necessary because lines is already a const ref...)
+                  - My part one had a bug, but I was "lucky" with my puzzle input so I passed by sheer luck..
+                  - My part two also had a bug; and I was also "lucky" with my puzzle input again and didn't catch it because I passed...  
+                    I didn't consider I have to save the row in NumInfo, without that two different numbers might compare equal when they aren't (which can lead to misclassification of gears)
+        - Use std::ssize instead of casting to int where necessary
+        - Use std::isdigit
         - I wonder which puzzle will be the first one to overflow int or int64_t...
 
 */
 
-static bool is_digit(const char c)
-{
-    return c >= '0' && c <= '9'; 
-}
-
 static int digit_char_to_int(const char c)
 {
-    if (!is_digit(c)) {
+    if (!std::isdigit(c)) {
         throw "char is not a digit";
     }
     return c - '0'; 
@@ -38,20 +39,20 @@ static bool idx_in_range(int num_rows, int num_cols, int row, int col)
 
 static bool adjacent_to_symbol(const std::vector<std::string>& lines, int row, int col)
 {
-    if (row < 0 || row >= (int)lines.size()) {
+    if (row < 0 || row >= std::ssize(lines)) {
         throw "row-index out of range"; 
     }
-    if (col < 0 || col >= (int)lines[row].size()) {
+    if (col < 0 || col >= std::ssize(lines.at(row))) {
         throw "col-index out of range";
     }
-    int num_rows = lines.size();
-    int num_cols = lines.at(row).size(); 
+    const int num_rows = lines.size();
+    const int num_cols = lines.at(row).size(); 
 
     for (int dx = -1; dx <= 1; ++dx) {
         for (int dy = -1; dy <= 1; ++dy) {
             if ((dx == 0 && dy == 0) || !idx_in_range(num_rows, num_cols, row + dy, col + dx)) {
                 continue;
-            } else if (lines.at(row + dy).at(col + dx) != '.' && !is_digit(lines.at(row + dy).at(col + dx)) ) {
+            } else if (lines.at(row + dy).at(col + dx) != '.' && !std::isdigit(lines.at(row + dy).at(col + dx)) ) {
                 return true; 
             }
         }
@@ -62,17 +63,17 @@ static bool adjacent_to_symbol(const std::vector<std::string>& lines, int row, i
 int part_one(const std::vector<std::string>& lines)
 {
     int part_sum = 0; 
-    for (int row = 0; row < (int)lines.size(); ++row) {
+    for (int row = 0; row < std::ssize(lines); ++row) {
         int num_scale = 1;
         int current_num = 0; 
         bool num_is_adj = false; 
-        for (int col = lines.at(row).size() - 1; col >= 0; --col) { // From right to left.
+        for (int col = std::ssize(lines.at(row)) - 1; col >= 0; --col) { // From right to left.
             char c = lines.at(row).at(col); 
-            if (is_digit(c)) {
+            if (std::isdigit(c)) {
                 num_is_adj = adjacent_to_symbol(lines, row, col) || num_is_adj; 
                 current_num += num_scale * digit_char_to_int(c); 
                 num_scale *= 10; 
-                if (col == 0) {
+                if (col == 0 && num_is_adj) { // NOTE: Forgot the && num_is_adj but my solution worked for my puzzle input, so I didn't notice... 
                     part_sum += current_num; 
                 }
             } else {
@@ -90,35 +91,35 @@ int part_one(const std::vector<std::string>& lines)
 
 static int get_gear_ratio(const std::vector<std::string>& lines, int row, int col)
 {
-    if (row < 0 || row >= (int)lines.size()) {
+    if (row < 0 || row >= std::ssize(lines)) {
         throw "row-index out of range"; 
     }
-    if (col < 0 || col >= (int)lines.at(row).size()) {
+    if (col < 0 || col >=  std::ssize(lines.at(row))) {
         throw "col-index out of range";
     }
     const int num_rows = lines.size();
     const int num_cols = lines.at(row).size();
 
     struct NumInfo {
-        int num, begin_idx, end_idx; 
+        int num, col_begin_idx, row_idx; 
         bool operator==(const NumInfo&) const = default; // C++20
     };
 
-    auto parse_num = [lines, num_cols](int row, int col) -> NumInfo {
+    auto parse_num = [&lines, num_cols](int row, int col) -> NumInfo { // NOTE: reference-capture lines with &lines to avoid copying (yes, despite lines already being a const ref). 
         int begin_idx = col;
-        while (begin_idx < num_cols - 1 && is_digit(lines.at(row).at(begin_idx + 1))) {
+        while (begin_idx + 1 < num_cols && std::isdigit(lines.at(row).at(begin_idx + 1))) {
             ++begin_idx;
         }
         int num = digit_char_to_int(lines.at(row).at(begin_idx)); 
         int num_power = 10;
         int end_idx = begin_idx; 
-        while (end_idx - 1 >= 0 && is_digit(lines.at(row).at(end_idx - 1))) {
+        while (end_idx - 1 >= 0 && std::isdigit(lines.at(row).at(end_idx - 1))) {
             num += num_power * digit_char_to_int(lines.at(row).at(end_idx - 1));
             num_power *= 10;
             --end_idx;
         }
         assert(end_idx <= begin_idx);
-        return NumInfo {.num=num, .begin_idx=begin_idx, .end_idx=end_idx}; 
+        return NumInfo {.num=num, .col_begin_idx=begin_idx, .row_idx=row}; // NOTE: I have to save the row_idx, otherwise two different numbers might compare as equal when they aren't. 
     }; 
 
     std::vector<NumInfo> nums;  
@@ -127,8 +128,11 @@ static int get_gear_ratio(const std::vector<std::string>& lines, int row, int co
             if ((dx == 0 && dy == 0) || !idx_in_range(num_rows, num_cols, row + dy, col + dx)) {
                 continue;
             }   
-            if (is_digit(lines.at(row + dy).at(col + dx))) {
-                auto num = parse_num(row + dy, col + dx);
+            if (nums.size() > 2) { // Can't be a gear.
+                return 0;
+            }
+            if (std::isdigit(lines.at(row + dy).at(col + dx))) {
+                NumInfo num = parse_num(row + dy, col + dx);
                 if (std::find(nums.begin(), nums.end(), num) == nums.end()) { // Avoid duplicates. 
                     nums.push_back(num);
                 }
@@ -146,9 +150,8 @@ static int get_gear_ratio(const std::vector<std::string>& lines, int row, int co
 int64_t part_two(const std::vector<std::string>& lines)
 {
     int64_t gear_ratio_sum = 0; 
-
-    for (int row = 0; row < (int)lines.size(); ++row) {
-        for (int col = lines.at(row).size() - 1; col >= 0; --col) { // From right to left.
+    for (int row = 0; row < std::ssize(lines); ++row) {
+        for (int col = std::ssize(lines.at(row)) - 1; col >= 0; --col) { // From right to left.
             char c = lines.at(row).at(col); 
             if (c == '*') {
                 gear_ratio_sum += get_gear_ratio(lines, row, col);
---
+++ day-04/day-04.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include "../aocio/aocio.hpp"
+
+/*
+    Problem: https://adventofcode.com/2023/day/4
+
+    Solutions:
+        - Part 1: n/a
+        - Part 2: n/a
+
+    Notes: n/a
+*/
+
+int main()
+{
+    aocio::print_day();
+    // std::vector<std::string> lines;
+    // std::string_view fname = AOC_INPUT_PATH;
+    // bool file_loaded = aocio::file_getlines(fname, lines);
+    // if (!file_loaded) {
+    //     std::cerr << "Error: " << "File '" << fname << "' not found\n";
+    //     return -1;
+    // }
+
+    // try {
+    //     // int p1 = part_one(lines);  
+    //     // std::cout << "Part 1: " << p1 << "\n";
+    //     // int64_t p2 = part_two(lines);
+    //     // std::cout << "Part 2: " << p2 << "\n";
+    // } catch (const char* err) {
+    //     std::cerr << "Error: " << err << "\n";
+    //     return -1;
+    // }
+
+    std::cout << "Hello Day-04!\n";
+
+    return 0;
+}
