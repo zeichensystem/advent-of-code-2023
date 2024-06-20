9569ac84721cc7fbc2ea1cb89f11bd507805f6d4
jun <83899451+zeichensystem@users.noreply.github.com>
2024-06-20 14:18:46 +0200

    Solve Day 17 Part 2

--- day-17/day-17.cpp
+++ day-17/day-17.cpp
@@ -9,13 +9,14 @@
   
     Solutions: 
         - Part 1: 907
-        - Part 2: 
+        - Part 2: 1057
     Notes:  
         - Implemented a priority queue for aocutil
         - Manhattan distance does not seem to be a good heuristic here and runs even slower. (Or maybe I implemented something wrong...) 
         - Found out the way I combined hashes (i.e. h_combined = h1 ^ (h2 << 1), cf. the example code from  https://en.cppreference.com/w/cpp/utility/hash)
           was really bad. Using a hash_combine function I found produces better hashes and therefore makes code using maps etc. way faster.
           When using hash_combine, part 1 runs in ~0.5 seconds instead of ~6 seconds.
+        - Part 2: Harder than expected; off-by-one-error, the problem.
 */
 
 using aocutil::Vec2; 
@@ -71,23 +72,25 @@ std::pair<Vec2<int>, Vec2<int>> get_left_right(const Vec2<int>& dir)
     throw std::invalid_argument("get_left_right: Invalid direction");
 }
 
-std::vector<State> find_adjacent(const Grid<int>& grid, const State& s, int max_steps)
+std::vector<State> find_adjacent(const Grid<int>& grid, const State& s, int min_steps, int max_steps)
 {   
     std::vector<State> adj; 
 
-    const auto [left_dir, right_dir] = get_left_right(s.dir); 
-    Vec2<int> left_pos = s.pos + left_dir;
-    Vec2<int> right_pos = s.pos + right_dir;
+    if (s.straight_cnt >= min_steps) {
+        const auto [left_dir, right_dir] = get_left_right(s.dir); 
+        Vec2<int> left_pos = s.pos + left_dir;
+        Vec2<int> right_pos = s.pos + right_dir;
 
-    if (grid.pos_on_grid(left_pos)) {
-        adj.emplace_back(State{.pos = left_pos, .dir = left_dir, .straight_cnt = 0}); 
+        if (grid.pos_on_grid(left_pos)) {
+            adj.emplace_back(State{.pos = left_pos, .dir = left_dir, .straight_cnt = 1}); 
+        }
+
+        if (grid.pos_on_grid(right_pos)) {
+            adj.emplace_back(State{.pos = right_pos, .dir = right_dir, .straight_cnt = 1}); 
+        }
     }
 
-    if (grid.pos_on_grid(right_pos)) {
-        adj.emplace_back(State{.pos = right_pos, .dir = right_dir, .straight_cnt = 0}); 
-    }
-
-    if (s.straight_cnt + 1 < max_steps) {
+    if (s.straight_cnt < max_steps) {
         Vec2<int> straight_pos = s.pos + s.dir; 
         if (grid.pos_on_grid(straight_pos)) {
             adj.emplace_back(State{.pos = straight_pos, .dir = s.dir, .straight_cnt = s.straight_cnt + 1}); 
@@ -111,7 +114,7 @@ int no_heuristic(const Vec2<int>& start, const Vec2<int>& target) {
     cf. https://en.wikipedia.org/wiki/A*_search_algorithm (last retrieved 2024-06-20)
     cf. https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm (last retrieved 2024-06-20)
 */ 
-int find_shortest_path(const Grid<int>& grid, int max_straight_steps = 3, std::function<int(const Vec2<int>&, const Vec2<int>&)> heuristic = no_heuristic)
+int find_shortest_path(const Grid<int>& grid, int min_straight_steps = 0, int max_straight_steps = 3, std::function<int(const Vec2<int>&, const Vec2<int>&)> heuristic = no_heuristic)
 {
     constexpr int infinity = std::numeric_limits<int>::max();
     const Vec2<int> end_pos = {grid.width() - 1, grid.height() - 1}; 
@@ -119,8 +122,8 @@ int find_shortest_path(const Grid<int>& grid, int max_straight_steps = 3, std::f
     PrioQueue<State> queue; 
     std::unordered_map<State, int> cost; // The real cost from the start node to a given node.
 
-    const State start_r = {.pos = {0, 0}, .dir = dir_right}; 
-    const State start_d = {.pos = {0, 0}, .dir = dir_down};
+    const State start_r = {.pos = {0, 0}, .dir = dir_right, .straight_cnt = 0}; 
+    const State start_d = {.pos = {0, 0}, .dir = dir_down, .straight_cnt = 0};
 
     queue.insert(start_r, heuristic(start_r.pos, end_pos)); 
     queue.insert(start_d, heuristic(start_d.pos, end_pos));
@@ -133,14 +136,14 @@ int find_shortest_path(const Grid<int>& grid, int max_straight_steps = 3, std::f
         const State current = queue.extract_min().value();
         const int cost_current = cost.at(current);
 
-        if (current.pos == end_pos) {
+        if (current.pos == end_pos && current.straight_cnt >= min_straight_steps) {
             min_cost = std::min(cost_current, min_cost);
             continue;
         }
 
-        for (const State& adj: find_adjacent(grid, current, max_straight_steps)) {
+        for (const State& adj: find_adjacent(grid, current, min_straight_steps, max_straight_steps)) {
             const int cost_adj_min = cost.contains(adj) ? cost.at(adj) : infinity;
-            if (const int cost_new = cost_current + grid[adj.pos]; cost_new <= cost_adj_min) {
+            if (const int cost_new = cost_current + grid.at(adj.pos); cost_new <= cost_adj_min) {
                 cost.insert_or_assign(adj, cost_new);
                 queue.insert_or_update(adj, cost_new + heuristic(adj.pos, end_pos));
             }
@@ -154,12 +157,14 @@ int part_one(const std::vector<std::string>& lines)
 {
     Grid<int> grid; 
     parse_grid(lines, grid); 
-    return find_shortest_path(grid, 3, no_heuristic);
+    return find_shortest_path(grid, 0, 3, no_heuristic);
 }
 
 int part_two(const std::vector<std::string>& lines)
 {
-    return -1;  
+    Grid<int> grid;
+    parse_grid(lines, grid);
+    return find_shortest_path(grid, 4, 10, no_heuristic);
 }
 
 int main()
