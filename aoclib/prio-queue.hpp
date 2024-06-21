d52bfaf8a91ab036841b1b97776bfbcbfbd08468
jun <83899451+zeichensystem@users.noreply.github.com>
2024-06-21 19:41:03 +0200

    Optimise Day 17

--- aoclib/prio-queue.hpp
+++ aoclib/prio-queue.hpp
@@ -6,6 +6,7 @@
 
 namespace aocutil 
 {
+
 template<typename T, typename PrioType = int>
 class PrioQueue 
 {
@@ -45,16 +46,30 @@ public:
         }
     }
 
-    std::optional<T> extract_min()
+    T extract_min()
     {
         auto min_elem = prio_to_elem.begin(); 
         if (min_elem == prio_to_elem.end()) { // Queue is empty.
             assert(empty());
-            return {}; 
+            throw std::out_of_range("PrioQueue extract_min: Queue already empty"); 
         }
         T elem = min_elem->second; 
         prio_to_elem.erase(min_elem);
         elem_to_prio.erase(elem); 
+        return elem;
+    }
+
+    T extract_min(PrioType& prio)
+    {
+        auto min_elem = prio_to_elem.begin(); 
+        if (min_elem == prio_to_elem.end()) { // Queue is empty.
+            assert(empty());
+            throw std::out_of_range("PrioQueue extract_min: Queue already empty"); 
+        }
+        T elem = min_elem->second; 
+        prio_to_elem.erase(min_elem);
+        elem_to_prio.erase(elem); 
+        prio = min_elem->first;
         return elem;
     }
 
@@ -75,4 +90,269 @@ public:
         return empt;
     }
 };
+
+// Is slower...
+// template<typename T, typename HashFun = void>
+// class BinaryHeap 
+// {
+//     enum class Child {Left, Right};
+//     std::vector<T> nodes; 
+//     using MapType = typename std::conditional_t<std::is_same_v<HashFun, void>, phmap::flat_hash_map<T, std::size_t>, phmap::flat_hash_map<T, std::size_t, HashFun>>;
+//     MapType elem_to_idx; 
+
+//     std::size_t get_parent_idx(std::size_t idx) const {
+//         return (idx - 1) / 2;
+//     }
+
+//     std::optional<std::size_t> get_children_idx(std::size_t idx, Child ct) const 
+//     {
+//         std::size_t child_idx = 0;
+//         if (ct == Child::Left) {
+//             child_idx = 2 * idx + 1;
+//         } else if (ct == Child::Right) {
+//             child_idx = 2 * idx + 2; 
+//         } else {
+//             assert(false);
+//         }
+        
+//         if (child_idx < nodes.size()) {
+//             return child_idx; 
+//         } else {
+//             return {};
+//         }
+//     }
+
+//     bool is_ordered(const T& parent, const T& child) const 
+//     {
+//         if (type == BinaryHeapType::Min) {
+//             return parent <= child;
+//         } else if (type == BinaryHeapType::Max) {
+//             return parent >= child;
+//         } else {
+//             throw std::invalid_argument("Binary heap is_ordered: Invalid binary heap type");
+//         }
+//     }
+
+
+//     void swap_nodes(std::size_t idx_a, std::size_t idx_b) {
+//         assert(elem_to_idx.contains(nodes.at(idx_a)));
+//         assert(elem_to_idx.contains(nodes.at(idx_b)));
+
+//         elem_to_idx.at(nodes.at(idx_a)) = idx_b; 
+//         elem_to_idx.at(nodes.at(idx_b)) = idx_a; 
+//         std::swap(nodes.at(idx_a), nodes.at(idx_b));
+//     }
+
+//     void heapify_up(std::size_t idx) 
+//     {
+//         if (nodes.size() <= 1) {
+//             return;
+//         }
+
+//         while (idx > 0 && !is_ordered(nodes.at(get_parent_idx(idx)), nodes.at(idx))) {
+//             swap_nodes(get_parent_idx(idx), idx);
+//             idx = get_parent_idx(idx);
+//         }
+//     }
+
+//     void heapify_down(std::size_t idx) 
+//     {
+//         if (nodes.size() <= 1) {
+//             return;
+//         }
+//         do {
+//             auto idx_left_child = get_children_idx(idx, Child::Left);
+//             auto idx_right_child = get_children_idx(idx, Child::Right);
+//             if (!idx_left_child) {
+//                 assert(!idx_right_child);
+//                 return;
+//             }
+//             if (!idx_right_child) {
+//                 idx_right_child = idx_left_child;
+//             }
+
+//             assert(idx_left_child && idx_right_child);
+//             const T& left = nodes.at(idx_left_child.value()); 
+//             const T& right = nodes.at(idx_right_child.value()); 
+//             const T& min_max_child = (type == BinaryHeapType::Min) ? std::min(left, right) : std::max(left, right); 
+//             std::size_t min_max_child_idx = min_max_child == left ? idx_left_child.value() : idx_right_child.value();
+
+//             if (!is_ordered(nodes.at(idx), nodes.at(min_max_child_idx))) {
+//                 swap_nodes(idx, min_max_child_idx);
+//                 idx = min_max_child_idx;
+//             } else {
+//                 break;
+//             }
+//         } while (idx < nodes.size() - 1);
+//     }
+
+//     bool is_heap() const {
+//         return (type == BinaryHeapType::Min) ? std::is_heap(nodes.cbegin(), nodes.cend(), std::greater<T>{}) : std::is_heap(nodes.begin(), nodes.end());
+//     }
+
+// public: 
+//     enum class BinaryHeapType {Max, Min};
+//     const BinaryHeapType type =  BinaryHeapType::Min;
+    
+//     BinaryHeap(BinaryHeapType type = BinaryHeapType::Min) :type(type) {};
+
+//     void insert(const T& elem) 
+//     {
+//         if (elem_to_idx.contains(elem)) {
+//             throw "BinaryHeap insert: elem already inside";
+//         }
+
+//         nodes.push_back(elem);
+//         std::size_t idx = nodes.size() - 1; 
+//         elem_to_idx.insert({elem, idx});
+
+//         heapify_up(idx);
+//         assert(is_heap());
+//     }
+
+//     T extract_min() 
+//     {
+//         if (size() == 0) {
+//             throw std::out_of_range("BinaryHeap extract_min: Heap is empty");
+//         }
+//         T extracted_elem = nodes.at(0);
+//         std::size_t last_idx = nodes.size() - 1;
+//         if (nodes.size() > 1) {
+//             swap_nodes(0, last_idx);
+//         }
+//         nodes.pop_back();
+
+   
+//         heapify_down(0);
+
+//         elem_to_idx.erase(extracted_elem);
+
+//         assert(is_heap());
+//         return extracted_elem;
+//     }
+
+//     void delete_elem(const T& elem) 
+//     {
+
+//         // cf. https://en.wikipedia.org/wiki/Binary_heap#Delete (last retrieved 2024-06-20)
+//         if (!elem_to_idx.contains(elem)) {
+//             throw std::out_of_range("BinaryHeap delete_elem: elem not in heap");
+//         }
+//         std::size_t deleted_idx = elem_to_idx.at(elem);
+//         std::size_t last_idx = nodes.size() - 1;
+        
+//         if (nodes.size() == 1) {
+//             elem_to_idx.erase(elem);
+//             nodes.pop_back();
+//             assert(is_heap());
+//             return;
+//         } 
+
+//         swap_nodes(deleted_idx, last_idx);
+//         bool require_heapify_up = (type == BinaryHeapType::Min) ? nodes.at(deleted_idx) < nodes.at(last_idx) : nodes.at(deleted_idx) > nodes.at(last_idx) ;
+
+//         elem_to_idx.erase(nodes.at(last_idx));
+//         nodes.pop_back();
+//         if (nodes.size() == 0) {
+//             assert(is_heap());
+//             return;
+//         }
+
+//         if (require_heapify_up) {
+//             heapify_up(deleted_idx);
+//         } else {
+//             heapify_down(deleted_idx);
+//         }
+//         assert(is_heap());
+//     }
+
+//     typename std::vector<T>::const_iterator cbegin() const {
+//         return nodes.cbegin();
+//     }
+
+//     typename std::vector<T>::const_iterator cend() const {
+//         return nodes.cend();
+//     }
+
+//     std::size_t size() const {
+//         return nodes.size();
+//     }
+
+//     bool contains(const T& elem) const {
+//         return elem_to_idx.contains(elem);
+//     }
+// };
+
+// template<typename T, typename PrioType = int>
+// class PrioQueueHeap 
+// {
+// private:
+//     struct KeyVal {
+//         PrioType key; 
+//         T val; 
+//         auto operator<=>(const KeyVal& other) const {return key - other.key;};
+//         auto operator==(const KeyVal& other) const {return val == other.val;};
+//         // KeyVal() = default;
+//         KeyVal(PrioType key, const T& val) :key(key), val(val) {};        
+//         KeyVal(const T& val) : val(val) {};
+
+
+//         struct KeyValHash {
+//             std::size_t operator()(const KeyVal& kv) const noexcept {
+//                     return std::hash<T>{}(kv.val); 
+//             }
+//         };
+//     };
+
+//     BinaryHeap<KeyVal, typename KeyVal::KeyValHash> heap;
+
+// public:
+//     void insert(const T& elem, const PrioType& priority)
+//     {
+//         KeyVal kv(priority, elem);
+//         if (heap.contains(kv)) {
+//             throw std::invalid_argument("PrioQueueHeap insert: Element already in queue"); 
+//         }
+//         heap.insert(kv);
+//     } 
+
+//     void update_prio(const T& elem, const PrioType& new_priority)
+//     {
+//         KeyVal kv(new_priority, elem);
+
+//         if (!heap.contains(kv)) {
+//             throw std::out_of_range("PrioQueueHeap update_prio: Element not in queue.");
+//         }
+//         heap.delete_elem(kv);
+//         heap.insert(kv);
+//     }
+
+//     void insert_or_update(const T& elem, const PrioType& prio)
+//     {
+//         if (heap.contains(KeyVal(prio, elem))) {
+//             update_prio(elem, prio); 
+//         } else {
+//             insert(elem, prio);
+//         }
+//     }
+
+//     T extract_min()
+//     {
+//         return heap.extract_min().val;
+//     }
+
+//     bool contains(const T& elem) const {
+//         return heap.contains(KeyVal(elem)); // The key stays undefined but that's okay.
+//     }
+
+//     std::size_t size() const {
+//         return heap.size();
+//     }
+
+//     bool empty() const 
+//     {
+//         return heap.size() == 0;
+//     }
+// };
+
 }
--- day-17/day-17.cpp
+++ day-17/day-17.cpp
@@ -1,7 +1,7 @@
-#include <unordered_set>
+#include <queue>
+#include <array>
 #include "../aoclib/aocio.hpp"
 #include "../aoclib/grid.hpp"
-#include "../aoclib/prio-queue.hpp"
 
 /*
     Problem: https://adventofcode.com/2023/day/17
@@ -16,11 +16,12 @@
           was really bad. Using a hash_combine function I found produces better hashes and therefore makes code using maps etc. way faster.
           When using hash_combine, part 1 runs in ~0.5 seconds instead of ~6 seconds.
         - Part 2: Harder than expected; off-by-one-error, the problem.
+
+        - Above does not apply to the optimised solution, because it does without unordered_maps. 
 */
 
 using aocutil::Vec2; 
 using aocutil::Grid; 
-using aocutil::PrioQueue;
 
 constexpr Vec2<int> dir_right = {.x = 1, .y = 0};
 constexpr Vec2<int> dir_left = {.x = -1, .y = 0};
@@ -40,20 +41,17 @@ void parse_grid(const std::vector<std::string>& lines, Grid<int>& grid)
     }
 }
 
+constexpr int infinity = std::numeric_limits<int>::max();
+
 struct State {
     Vec2<int> pos, dir; 
     int straight_cnt; 
-    bool operator==(const State&) const = default;
-};
 
-template<>
-struct std::hash<State> {
-    std::size_t operator()(const State& s) const noexcept
-    {
-        std::size_t h = 0; 
-        aocutil::hash_combine(h, s.pos, s.dir, s.straight_cnt);
-        return h;
-    }
+    int cost = infinity;
+    bool visited = false;
+
+    auto operator<=>(const State& other) const {return cost - other.cost;};
+    bool operator==(const State& other) const {return pos == other.pos && dir == other.dir && straight_cnt == other.straight_cnt;};
 };
 
 std::pair<Vec2<int>, Vec2<int>> get_left_right(const Vec2<int>& dir)
@@ -71,9 +69,55 @@ std::pair<Vec2<int>, Vec2<int>> get_left_right(const Vec2<int>& dir)
     throw std::invalid_argument("get_left_right: Invalid direction");
 }
 
-std::vector<State> find_adjacent(const Grid<int>& grid, const State& s, int min_steps, int max_steps)
+
+constexpr int max_straight_steps_limit = 11; 
+constexpr int num_directions = 4; 
+constexpr int max_states_per_cell = num_directions * max_straight_steps_limit; 
+
+int calc_state_idx(const State& s)
+{
+    int idx = 0; 
+    if (s.dir == dir_up) {
+        idx = 1 * max_straight_steps_limit; 
+    } else if (s.dir == dir_right) {
+        idx = 2 * max_straight_steps_limit; 
+    } else if (s.dir == dir_down) {
+        idx = 3 * max_straight_steps_limit;
+    }
+    idx += s.straight_cnt; 
+    assert(idx < max_states_per_cell); 
+    return idx; 
+}
+
+struct StateCell 
+{
+    std::array<State, max_states_per_cell> states; 
+    std::size_t size = 0; 
+
+    using StateArrIter = typename decltype(states)::iterator; 
+
+    StateArrIter insert(const State& s)
+    {
+        assert(size < states.size()); 
+        states[size] = s; 
+        return states.begin() + (size++);
+    }
+
+    std::optional<StateArrIter> find(const State& s)
+    {
+        auto end_it = states.begin() + size; 
+        auto it = std::find(states.begin(), end_it, s); 
+        if (it != end_it) {
+            return it; 
+        } else {
+            return {}; 
+        }
+    }
+};
+
+std::vector<StateCell::StateArrIter> find_adjacent(const Grid<int>& grid, Grid<StateCell>& state_grid, const State& s, int min_steps, int max_steps)
 {   
-    std::vector<State> adj; 
+    std::vector<StateCell::StateArrIter> adj; 
 
     if (s.straight_cnt >= min_steps) {
         const auto [left_dir, right_dir] = get_left_right(s.dir); 
@@ -81,70 +125,88 @@ std::vector<State> find_adjacent(const Grid<int>& grid, const State& s, int min_
         Vec2<int> right_pos = s.pos + right_dir;
 
         if (grid.pos_on_grid(left_pos)) {
-            adj.emplace_back(State{.pos = left_pos, .dir = left_dir, .straight_cnt = 1}); 
+            State state {.pos = left_pos, .dir = left_dir, .straight_cnt = 1, .cost = infinity, .visited = false};
+            auto found = state_grid.at(state.pos).find(state);
+            if (found) {
+                adj.push_back(found.value());
+            } else {
+                adj.push_back(state_grid.at(state.pos).insert(state));
+            }
         }
 
         if (grid.pos_on_grid(right_pos)) {
-            adj.emplace_back(State{.pos = right_pos, .dir = right_dir, .straight_cnt = 1}); 
+            State state {.pos = right_pos, .dir = right_dir, .straight_cnt = 1, .cost = infinity, .visited = false}; 
+            auto found = state_grid.at(state.pos).find(state);
+            if (found) {
+                adj.push_back(found.value());
+            } else {
+                adj.push_back(state_grid.at(state.pos).insert(state));
+            }
         }
     }
 
     if (s.straight_cnt < max_steps) {
         Vec2<int> straight_pos = s.pos + s.dir; 
         if (grid.pos_on_grid(straight_pos)) {
-            adj.emplace_back(State{.pos = straight_pos, .dir = s.dir, .straight_cnt = s.straight_cnt + 1}); 
+            State state {.pos = straight_pos, .dir = s.dir, .straight_cnt = s.straight_cnt + 1, .cost = infinity, .visited = false}; 
+            auto found = state_grid.at(state.pos).find(state);
+            if (found) {
+                adj.push_back(found.value());
+            } else {
+                adj.push_back(state_grid.at(state.pos).insert(state));
+            }
         }
     }
 
     return adj;
 }
 
-int manhattan_dist(const Vec2<int>& start, const Vec2<int>& target) { // A heuristic is admissible if it never overestimates the actual cost to get to the target. 
-    return std::abs(start.x - target.x) + std::abs(start.y - target.y);
-}
-
-int no_heuristic(const Vec2<int>& start, const Vec2<int>& target) {
-    return 0; 
-}
-
-/*
-    Implements an A* search algorithm.
-    If no_heuristic is used for the heuristic parameter, A* is equivalent to Dijkstra's algorithm. 
-    cf. https://en.wikipedia.org/wiki/A*_search_algorithm (last retrieved 2024-06-20)
-    cf. https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm (last retrieved 2024-06-20)
-*/ 
-int find_shortest_path(const Grid<int>& grid, int min_straight_steps = 0, int max_straight_steps = 3, std::function<int(const Vec2<int>&, const Vec2<int>&)> heuristic = no_heuristic)
+int find_shortest_path(const Grid<int>& grid, int min_straight_steps = 0, int max_straight_steps = 3)
 {
-    constexpr int infinity = std::numeric_limits<int>::max();
     const Vec2<int> end_pos = {grid.width() - 1, grid.height() - 1}; 
 
-    PrioQueue<State> queue; 
-    std::unordered_map<State, int> cost; // The real cost from the start node to a given node.
+    State start_r = {.pos = {0, 0}, .dir = dir_right, .straight_cnt = 0, .cost = 0, .visited = false}; 
+    State start_d = {.pos = {0, 0}, .dir = dir_down, .straight_cnt = 0, .cost = 0, .visited = false};
 
-    const State start_r = {.pos = {0, 0}, .dir = dir_right, .straight_cnt = 0}; 
-    const State start_d = {.pos = {0, 0}, .dir = dir_down, .straight_cnt = 0};
+    std::priority_queue<State, std::vector<State>, std::greater<State>> queue; 
+    queue.push(start_r);
+    queue.push(start_d);
 
-    queue.insert(start_r, heuristic(start_r.pos, end_pos)); 
-    queue.insert(start_d, heuristic(start_d.pos, end_pos));
-    cost.insert({start_r, 0}); 
-    cost.insert({start_d, 0});
+    Grid<StateCell> state_grid;
+    for (int y = 0; y < grid.height(); ++y) {
+        state_grid.push_row(std::vector<StateCell>(grid.width(), StateCell{.size = 0}));
+    }
+
+    state_grid.at(start_r.pos).insert(start_r);
+    state_grid.at(start_d.pos).insert(start_d);
 
     int min_cost = infinity; 
-
+    
     while (!queue.empty()) {
-        const State current = queue.extract_min().value();
-        const int cost_current = cost.at(current);
+        const State current = queue.top();
+        queue.pop();
+
+        auto current_ref = state_grid.at(current.pos).find(current);
+        assert(current_ref); 
+
+        if (current_ref.value()->visited) {
+            continue;
+        } 
+        current_ref.value()->visited = true;
 
         if (current.pos == end_pos && current.straight_cnt >= min_straight_steps) {
-            min_cost = std::min(cost_current, min_cost);
-            continue;
+            std::cout << "q: " << queue.size() << "\n";
+            return current.cost;
         }
+    
 
-        for (const State& adj: find_adjacent(grid, current, min_straight_steps, max_straight_steps)) {
-            const int cost_adj_min = cost.contains(adj) ? cost.at(adj) : infinity;
-            if (const int cost_new = cost_current + grid.at(adj.pos); cost_new <= cost_adj_min) {
-                cost.insert_or_assign(adj, cost_new);
-                queue.insert_or_update(adj, cost_new + heuristic(adj.pos, end_pos));
+        for (StateCell::StateArrIter adj: find_adjacent(grid, state_grid, current, min_straight_steps, max_straight_steps)) {
+            if (adj->visited) {
+                continue;
+            }
+            if (int new_cost = current.cost + grid.at(adj->pos); new_cost <= adj->cost) {
+                adj->cost = new_cost;
+                queue.push(*adj);
             }
         }
     }
@@ -156,14 +218,14 @@ int part_one(const std::vector<std::string>& lines)
 {
     Grid<int> grid; 
     parse_grid(lines, grid); 
-    return find_shortest_path(grid, 0, 3, no_heuristic);
+    return find_shortest_path(grid, 0, 3);
 }
 
 int part_two(const std::vector<std::string>& lines)
 {
     Grid<int> grid;
     parse_grid(lines, grid);
-    return find_shortest_path(grid, 4, 10, no_heuristic);
+    return find_shortest_path(grid, 4, 10);
 }
 
 int main()
